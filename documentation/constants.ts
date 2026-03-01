d2599a19d7270692d32ce7e546d1b2232a525763
Will Christenson <will@wlcr.io>
2026-03-01 12:19:31 -0800

    More colors

--- documentation/constants.ts
+++ documentation/constants.ts
@@ -9,7 +9,8 @@ export const NAV_ITEMS: Array<{ page: Page; label: string }> = [
 ];
 
 export const ACCENT_COLORS: AccentColor[] = [
-  'blue', 'indigo', 'violet', 'pink', 'red', 'orange', 'amber', 'yellow', 'lime', 'green', 'teal', 'cyan', 'black',
+  'blue', 'indigo', 'purple', 'violet', 'pink', 'ruby', 'red', 'orange',
+  'amber', 'yellow', 'lime', 'green', 'jade', 'teal', 'cyan', 'sky', 'black',
 ];
 
 export const GRAY_COLORS: GrayColor[] = ['gray', 'mauve', 'slate', 'sage', 'olive', 'sand'];
@@ -25,8 +26,9 @@ export const SCALINGS: Scaling[] = [
 export const APPEARANCES: Appearance[] = ['light', 'dark', 'inherit'];
 
 export const ALL_HUES = [
-  'gray', 'mauve', 'slate', 'sage', 'olive', 'sand', 'blue', 'indigo', 'violet', 'pink',
-  'red', 'orange', 'amber', 'yellow', 'lime', 'green', 'teal', 'cyan',
+  'gray', 'mauve', 'slate', 'sage', 'olive', 'sand', 'blue', 'indigo', 'purple', 'violet',
+  'pink', 'ruby', 'red', 'orange', 'amber', 'yellow', 'lime', 'green',
+  'jade', 'teal', 'cyan', 'sky',
 ];
 
 export const STEPS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
@@ -34,16 +36,20 @@ export const STEPS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 export const ACCENT_SWATCH_COLORS: Record<string, string> = {
   blue: 'var(--color-blue-9)',
   indigo: 'var(--color-indigo-9)',
+  purple: 'var(--color-purple-9)',
   violet: 'var(--color-violet-9)',
   pink: 'var(--color-pink-9)',
+  ruby: 'var(--color-ruby-9)',
   red: 'var(--color-red-9)',
   orange: 'var(--color-orange-9)',
   amber: 'var(--color-amber-9)',
   yellow: 'var(--color-yellow-9)',
   lime: 'var(--color-lime-9)',
   green: 'var(--color-green-9)',
+  jade: 'var(--color-jade-9)',
   teal: 'var(--color-teal-9)',
   cyan: 'var(--color-cyan-9)',
+  sky: 'var(--color-sky-9)',
   black: 'var(--color-black-9)',
 };
 
